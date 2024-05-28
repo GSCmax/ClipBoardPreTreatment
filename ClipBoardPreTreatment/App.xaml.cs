32ac7ae11a3d4956f6c7d10610000f25a6043607
gsc <gsc067218@gmail.com>
2024-05-28 20:04:31 +0800

    一些优化

--- ClipBoardPreTreatment/App.xaml.cs
+++ ClipBoardPreTreatment/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using ClipBoardPreTreatment.Tools;
 using Hardcodet.Wpf.TaskbarNotification;
 using System.Diagnostics;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Data;
 
 namespace ClipBoardPreTreatment
 {
@@ -32,6 +34,22 @@ namespace ClipBoardPreTreatment
 
             //保存配置
             GlobalDataHelper.Save();
+        }
+    }
+
+    /// <summary>
+    /// 用于Tooltip的启用/停用显示
+    /// </summary>
+    public class Bool2StringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? "启用" : "停用";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
